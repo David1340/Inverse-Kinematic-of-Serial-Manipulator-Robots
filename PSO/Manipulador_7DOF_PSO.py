815954cbba03d1270ba717b6a98a3f101e85c1eb
David1340 <davidnike80@gmail.com>
2021-03-17 16:36:10 -0300

    Melhorei o plot e coloquei limite de velocidade no PSO

--- PSO/Manipulador_7DOF_PSO.py
+++ PSO/Manipulador_7DOF_PSO.py
@@ -1,3 +1,4 @@
+#PSO aplicado ao pionner 3D
 from random import random,uniform
 from math import pi,cos,sin,sqrt,atan2
 import numpy as np
@@ -5,9 +6,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 
 def matriz_homogenea(d,a,alfa,theta):
-    L1 = np.array([cos(theta), -sin(theta)*cos(alfa), sin(theta)*sin(alfa), a*cos(theta)])
-    L2 = np.array([sin(theta), cos(theta)*cos(alfa), -cos(theta)*sin(alfa), a*sin(theta)])
-    L3 = np.array([0, sin(alfa), cos(alfa), d])
+    #retorna a matriz homogênea a partir do parâmetros de DH
+    c = 5 #numero de casas decimais
+    L1 = np.array([round(cos(theta),c), round(-sin(theta)*cos(alfa),c),\
+        round(sin(theta)*sin(alfa),c),round(a*cos(theta),c)])
+    L2 = np.array([round(sin(theta),c), round(cos(theta)*cos(alfa),c),\
+        round(-cos(theta)*sin(alfa),c),round(a*sin(theta),c)])
+    L3 = np.array([0, round(sin(alfa),c), round(cos(alfa),c), d])
     L4 = np.array([0,0,0,1])
     A = np.array([L1,L2,L3,L4])
     return A
@@ -19,12 +24,28 @@ def distancia(a,b,n):
     return sqrt(d)
 
 def orientação(A):
-    #Z-Y-X
+     #calcular os ângulos de orientação na conversão Z -> Y -> X
     R = atan2(A[1,0],A[0,0])
     P = atan2(-A[2,0],sqrt((A[2,1]**2)+(A[2,2]**2)))
     Y = atan2(A[2,1],A[2,2])
     result = np.array([R,P,Y])
     return result
+
+def plot_junta_revolucao(A,p):
+    #A matriz de Rotação, p origem da junta no seu sistema de coordenadas
+    r = 0.025
+    h = 0.05
+    theta = np.arange(0,2*pi + 0.12,0.4) #theta de 0 a 2pi com passos de 0.1
+    z = np.linspace(-h,h,np.size(theta))#z de 0.1 a 0.1 com o numero de elementos iguais ao de theta
+    z, theta = np.meshgrid(z, theta) #transforma em vetores 2D por causa do plot de superficie
+    #[x,y,z].T = A@([x,y,z].T + p)
+    x = (r*np.cos(theta) + p[0,0])*A[0,0] + (r*np.sin(theta) + p[1,0])*A[0,1] + \
+     (z + p[2,0])*A[0,2] + np.ones_like(z)*A[0,3]
+    y = (r*np.cos(theta) + p[0,0])*A[1,0] + (r*np.sin(theta) + p[1,0])*A[1,1] + \
+     (z + p[2,0])*A[1,2] + np.ones_like(z)*A[1,3]
+    z = (r*np.cos(theta) + p[0,0])*A[2,0] + (r*np.sin(theta) + p[1,0])*A[2,1] + \
+     (z + p[2,0])*A[2,2] + np.ones_like(z)*A[2,3]
+    ax.plot_surface(x, y, z,color = 'blue', alpha = 1)
 
 def plot(qbest,t):
     #Comprimento dos elos do manipulador
@@ -47,9 +68,9 @@ def plot(qbest,t):
     p3_3 = np.array([[0,-b4,0,1]]).T #junta3
     p4_4 = p #junta4
     p5_5 = np.array([[0,-b6,0,1]]).T #junta5
-    p6_6 = np.array([[-b7,0,0,1]]).T#junta6
-    p7_7 = p#junta7
-    p8_7 = np.array([[0,0,L,1]]).T#contato de atuação do efetuador
+    p6_6 = np.array([[-b7,0,0,1]]).T #junta6
+    p7_7 = p #junta7
+    p8_7 = np.array([[0,0,L,1]]).T #contato de atuação do efetuador
     
     #O efetuador será um u, para plotarmos um precisamos de 4 pontos
     #topo esquerdo do u
@@ -98,62 +119,66 @@ def plot(qbest,t):
     A6 = matriz_homogenea(d6,a6,alpha6,theta6)
     A7 = matriz_homogenea(d7,a7,alpha7,theta7)
     #Calculando os pontos de interesse no sistema Global
-    p1_0 = A1@p1_1
-    p2_0 = A1@(A2@p2_2)
-    p3_0 = A1@(A2@(A3@p3_3))
-    p4_0 = A1@(A2@(A3@(A4@p4_4)))
-    p5_0 = A1@(A2@(A3@(A4@(A5@p5_5))))
-    p6_0 = A1@(A2@(A3@(A4@(A5@(A6@p6_6)))))
-    p7_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(p7_7)))))))
-    p8_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(p8_7)))))))
-    e1_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(e1_7)))))))
-    e2_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(e2_7)))))))
-    e3_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(e3_7)))))))
-    e4_0 = A1@(A2@(A3@(A4@(A5@(A6@(A7@(e4_7)))))))
-    ax.clear()
+    T1 = A1
+    T2 = T1@A2
+    T3 = T2@A3
+    T4 = T3@A4
+    T5 = T4@A5
+    T6 = T5@A6
+    T7 = T6@A7
+    p1_0 = T1@p1_1
+    p2_0 = T2@p2_2
+    p3_0 = T3@p3_3
+    p4_0 = T4@p4_4
+    p5_0 = T5@p5_5
+    p6_0 = T6@p6_6
+    p7_0 = T7@p7_7
+    p8_0 = T7@p8_7
+    e1_0 = T7@e1_7
+    e2_0 = T7@e2_7
+    e3_0 = T7@e3_7
+    e4_0 = T7@e4_7
     
+    #Plotando Elos 
+    ax.clear()   
     #Plotando Elos    
-    plt.plot([p[0,0],p1_0[0,0]],[p[1,0],p1_0[1,0]],[p[2,0],p1_0[2,0]])
-    plt.plot([p1_0[0,0],p2_0[0,0]],[p1_0[1,0],p2_0[1,0]],[p1_0[2,0],p2_0[2,0]])
-    plt.plot([p2_0[0,0],p3_0[0,0]],[p2_0[1,0],p3_0[1,0]],[p2_0[2,0],p3_0[2,0]])
-    plt.plot([p3_0[0,0],p4_0[0,0]],[p3_0[1,0],p4_0[1,0]],[p3_0[2,0],p4_0[2,0]])
-    plt.plot([p4_0[0,0],p5_0[0,0]],[p4_0[1,0],p5_0[1,0]],[p4_0[2,0],p5_0[2,0]])
-    plt.plot([p5_0[0,0],p6_0[0,0]],[p5_0[1,0],p6_0[1,0]],[p5_0[2,0],p6_0[2,0]])
-    plt.plot([p6_0[0,0],p7_0[0,0]],[p6_0[1,0],p7_0[1,0]],[p6_0[2,0],p7_0[2,0]])
+    plt.plot([p[0,0],p1_0[0,0]],[p[1,0],p1_0[1,0]],[p[2,0],p1_0[2,0]],'blue')
+    plt.plot([p1_0[0,0],p2_0[0,0]],[p1_0[1,0],p2_0[1,0]],[p1_0[2,0],p2_0[2,0]],'blue')
+    plt.plot([p2_0[0,0],p3_0[0,0]],[p2_0[1,0],p3_0[1,0]],[p2_0[2,0],p3_0[2,0]],'blue')
+    plt.plot([p3_0[0,0],p4_0[0,0]],[p3_0[1,0],p4_0[1,0]],[p3_0[2,0],p4_0[2,0]],'blue')
+    plt.plot([p4_0[0,0],p5_0[0,0]],[p4_0[1,0],p5_0[1,0]],[p4_0[2,0],p5_0[2,0]],'blue')
+    plt.plot([p5_0[0,0],p6_0[0,0]],[p5_0[1,0],p6_0[1,0]],[p5_0[2,0],p6_0[2,0]],'blue')
+    plt.plot([p6_0[0,0],p7_0[0,0]],[p6_0[1,0],p7_0[1,0]],[p6_0[2,0],p7_0[2,0]],'blue')
     
     #Plotando efetuador
     plt.plot([e1_0[0,0],e2_0[0,0],e3_0[0,0],e4_0[0,0],e3_0[0,0],p8_0[0,0],p7_0[0,0]]\
              ,[e1_0[1,0],e2_0[1,0],e3_0[1,0],e4_0[1,0],e3_0[1,0],p8_0[1,0],p7_0[1,0]]\
-             ,[e1_0[2,0],e2_0[2,0],e3_0[2,0],e4_0[2,0],e3_0[2,0],p8_0[2,0],p7_0[2,0]])
-    #Plotando objetivo
+             ,[e1_0[2,0],e2_0[2,0],e3_0[2,0],e4_0[2,0],e3_0[2,0],p8_0[2,0],p7_0[2,0]],'blue')
     
-    ax.scatter(t[0],t[1],t[2])
+    #Plotando objetivo    
+    ax.scatter(t[0],t[1],t[2],'yellow')
     
     #Plotando Juntas e base    
-    ax.scatter(p[0,0],p[1,0],p[2,0])
-    ax.scatter(p1_0[0,0],p1_0[1,0],p1_0[2,0])
-    ax.scatter(p2_0[0,0],p2_0[1,0],p2_0[2,0])
-    ax.scatter(p3_0[0,0],p3_0[1,0],p3_0[2,0])
-    ax.scatter(p4_0[0,0],p4_0[1,0],p4_0[2,0])
-    ax.scatter(p5_0[0,0],p5_0[1,0],p5_0[2,0])
-    ax.scatter(p6_0[0,0],p6_0[1,0],p6_0[2,0])
-    ax.scatter(p7_0[0,0],p7_0[1,0],p7_0[2,0])
-    
-    #Legendas
-    plt.legend(['Elo 1','Elo 2','Elo 3','Elo4', 'Elo5'\
-            ,'Elo6','Elo7','Efetuador','objetivo'])
+    plot_junta_revolucao(np.eye(4),p)
+    plot_junta_revolucao(T1,p1_1)
+    plot_junta_revolucao(T2,p2_2)
+    plot_junta_revolucao(T3,p3_3)
+    plot_junta_revolucao(T4,p4_4)
+    plot_junta_revolucao(T5,p5_5)
+    plot_junta_revolucao(T6,p6_6)
+    plot_junta_revolucao(T7,p7_7)
 
     ax.set_xlabel('Eixo x(m)')
     ax.set_ylabel('Eixo y(m)')
     ax.set_zlabel('Eixo z(m)')
 
     #titulo
-    plt.title('Manipulador 7DOF')
+    plt.title('Pioneer 7DOF')
     ax.set_xlim3d(-0.8,0.8)
     ax.set_ylim3d(-0.8,0.8)
     ax.set_zlim3d(0,0.8)
     fig.canvas.draw() #mostra o plot
-    plt.pause(0.1)
+    plt.pause(0.05)
     
 class particle:
     def __init__(self,position,dimension):
@@ -169,13 +194,17 @@ class particle:
     def update_position(self,qbest,L): #Update the particle position
         #c1 = 1.4047
         #c2 = 1.494
-        c1 = 1
-        c2 = 2
+        c1 = 1 #grupo
+        c2 = 2 #individual
         for i in range(self.n):
             w = 0.5 + random()/2
             vmax = 0.5
             #w = random()
             self.v[i] = w*self.v[i] + c1*random()*(qbest[i] - self.p[i])+ c2*random()*(self.bp[i] - self.p[i])
+            if(self.v[i] > vmax):
+                self.v[i] = vmax
+            elif(self.v[i] < -vmax):
+                self.v[i] = -vmax
             self.p[i] = self.p[i] + self.v[i]
             if(self.p[i] > L[i]):
                 self.p[i] = L[i]
@@ -247,8 +276,8 @@ class particle:
         self.d = distancia(p.T,o,3)
 
         #função de custo       
-        k1 = 0.05
-        k2 = 1
+        k1 = 0.2 #orientação
+        k2 = 0.8 #posição
         self.f = (k1*self.o) + (k2*self.d)
         if(self.f < self.bf):
             self.bf = self.f
@@ -259,7 +288,7 @@ def PSO(o,o2,number,n,L):
     k = 250      
     q = []
     
-    #criando number particulas de dimensão n e calculando o valor de sua função de custo
+    #criando as particulas de dimensão n e calculando o valor de sua função de custo
     for i in range(number):
         p = np.array([uniform(-L[0],L[0]),uniform(-L[1],L[1]),uniform(-L[2],L[2]),uniform(-L[3],L[3])\
                       ,uniform(-L[4],L[4]),uniform(-L[5],L[5]),uniform(-L[6],L[6])])
@@ -302,12 +331,12 @@ ax =  fig.add_subplot(111, projection = '3d')
 plt.pause(0.1)
 
 #objetivo
-objetivo = np.array([0.2,0.5,0.6])
-objetivo2 = np.array([0,0,0])
-numero_particulas = 1200
-dimensao = 7
+objetivo = np.array([0.2,0.5,0.6]) #posiçãp
+objetivo2 = np.array([0,0,0]) #orientação
+numero_particulas = 200
+dimensao = 7 #dimensão do robô
 
-#constante limitadora dos angulos
+#restrições de cada ângulp angulos
 c = pi/12 
 L = [(pi)-c,pi/2,(pi)-c,(pi)-c,(pi)-c,(pi)-c,(pi)-c]
 solucao = PSO(objetivo,objetivo2,numero_particulas,dimensao,L)
