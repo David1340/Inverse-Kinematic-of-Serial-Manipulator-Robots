dd1c6e9b1ede2b48098fc1acca5be24406960f8b
david1340 <davidnike80@gmail.com>
2021-06-17 08:43:10 -0300

    Implementei amis algumas funcoes basicas que serao necessarias para implementar o Fabrik

--- Simulacoes_Ros/Heuristicos/Fabrik.py
+++ Simulacoes_Ros/Heuristicos/Fabrik.py
@@ -30,6 +30,41 @@ def iteracao_Fabrik(p,p0,d,normal):
     pnew = (1- delta)*p0 + delta*proj
     return pnew
 
+def multiplicacao_quaternios(q,q2):
+    resultado = np.array([[0,0,0,0]])
+    resultado[0,0] = q[0,0]*q2[0,0] -q[1,0]*q2[1,0] -q[2,0]*q2[2,0] -q[3,0]*q2[3,0] 
+    resultado[1,0] = q[0,0]*q2[1,0] -q[1,0]*q2[0,0] -q[2,0]*q2[3,0] -q[3,0]*q2[2,0] 
+    resultado[2,0] = q[0,0]*q2[2,0] -q[1,0]*q2[3,0] -q[2,0]*q2[0,0] -q[3,0]*q2[1,0] 
+    resultado[3,0] = q[0,0]*q2[3,0] -q[1,0]*q2[2,0] -q[2,0]*q2[1,0] -q[3,0]*q2[0,0] 
+    return resultado
+
+def rotationar_vetor(p,v,th):
+    #gira p em torno de v em th rad
+    a = cos(th/2)
+    b = v[[0,0]]*sin(th/2)
+    c = v[[1,0]]*sin(th/2)
+    d = v[[2,0]]*sin(th/2)
+    p_aumentado = np.zeros([4,1])
+    p_aumentado[0:3,0] = p[:,0]
+    h = np.array[[a,b,c,d]]
+    hx = np.array[[a,-b,-c,-d]]
+    p_r = multiplicacao_quaternios(h,p)
+    q_r = multiplicacao_quaternios(p_r,hx)
+    return q_r[0:3]
+
+#matriz_antissimetrica
+def S(a):
+    #A = [0,-az,ay ; az,0,-ax ; -ay,ax,0]
+    #Uso para calcular produto vetorial entre vetores u x v = S(u) * v
+    A = np.zeros((3,3))
+    A[0,1] = -a[2]
+    A[0,2] = a[1]
+    A[1,2] = -a[0]
+    A[1,0] = - A[0,1]
+    A[2,0] = - A[0,2]
+    A[2,1] = - A[1,2]
+    return A
+
 n = np.array([[0,0,1]]).T
 p = np.array([[5,4,3]]).T
 p0 = np.array([[1,1,6]]).T
