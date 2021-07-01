eddfb6546f8da54e57c5678e522315244e3b28dd
David1340 <davidnike80@gmail.com>
2021-07-01 18:29:22 -0300

    Terminei de implementar o CCD, falta melhorar o código e implementar ele no Rviz

--- Simulacoes_Matplotlib/Heuristicos/CCD.py
+++ Simulacoes_Matplotlib/Heuristicos/CCD.py
@@ -4,8 +4,31 @@ from math import cos, sin, sqrt, pi, acos,atan2
 import numpy as np
 import random 
 
+def multiplicacao_quaternios(q,q2):
+    resultado = np.zeros([4,1])
+    resultado[0,0] = q[0,0]*q2[0,0] -q[1,0]*q2[1,0] -q[2,0]*q2[2,0] -q[3,0]*q2[3,0] 
+    resultado[1,0] = q[0,0]*q2[1,0] +q[1,0]*q2[0,0] +q[2,0]*q2[3,0] -q[3,0]*q2[2,0] 
+    resultado[2,0] = q[0,0]*q2[2,0] -q[1,0]*q2[3,0] +q[2,0]*q2[0,0] +q[3,0]*q2[1,0] 
+    resultado[3,0] = q[0,0]*q2[3,0] +q[1,0]*q2[2,0] -q[2,0]*q2[1,0] +q[3,0]*q2[0,0] 
+    #print(resultado)
+    return resultado
+
+def rotationar_vetor(p,v,th):
+    #gira p em torno de v em th rad
+    a = cos(th/2)
+    b = v[0,0]*sin(th/2)
+    c = v[1,0]*sin(th/2)
+    d = v[2,0]*sin(th/2)
+    p_aumentado = np.zeros([4,1])
+    p_aumentado[1:4,0] = p[:,0]
+    h = np.array([[a,b,c,d]]).T
+    hx = np.array([[a,-b,-c,-d]]).T
+    p_r = multiplicacao_quaternios(h,p_aumentado)
+    q_r = multiplicacao_quaternios(p_r,hx)
+    return q_r[1:4]
+
 def acosr(x):
-    return acos(np.round(x,5))
+    return acos(np.round(x,10))
 
 def S(a):
     #A = [0,-az,ay ; az,0,-ax ; -ay,ax,0]
@@ -42,7 +65,7 @@ def matriz_homogenea(d,a,alfa,theta):
     L1 = np.array([cos(theta),-sin(theta)*cos(alfa),sin(theta)*sin(alfa),a*cos(theta)])
     L2 = np.array([sin(theta),cos(theta)*cos(alfa),-cos(theta)*sin(alfa),a*sin(theta)])
     L3 = np.array([0,sin(alfa),cos(alfa), d])
-    L4 = np.array([0,0,0,1])
+    L4 = np.array([0.0,0.0,0.0,1.0])
     A = np.array([L1,L2,L3,L4])
     return A
 
@@ -339,7 +362,8 @@ def Cinematica_Direta(q):
 plt.ion()
 fig = plt.figure()
 ax =  fig.add_subplot(111, projection = '3d')
-destino = np.array([[0.2,0.5,0.6]]).T
+#destino = np.array([[0.2,0.5,0.6]]).T
+destino = np.array([[0.5*random.random(),0.5*random.random(),0.6*random.random()]]).T
 q = np.array([[0.0,0.0,0.0,0.0,0.0,0.0,0.0]]).T
 
 #Comprimento dos elos do manipulador
@@ -375,56 +399,155 @@ alpha5 = pi/2
 alpha6 = pi/2
 alpha7 = pi/2
 
-### parametros de DH variáveis
-theta1 = pi/2 + q[0]
-theta2 = q[1]
-theta3 = q[2]
-theta4 = q[3]
-theta5 = q[4]
-theta6 = pi/2 + q[5]
-theta7 = pi/2 + q[6]
 
-#Calculando as matrizes homogêneas
-A1 = matriz_homogenea(d1,a1,alpha1,theta1)
-A2 = matriz_homogenea(d2,a2,alpha2,theta2)
-A3 = matriz_homogenea(d3,a3,alpha3,theta3)
-A4 = matriz_homogenea(d4,a4,alpha4,theta4)
-A5 = matriz_homogenea(d5,a5,alpha5,theta5)
-A6 = matriz_homogenea(d6,a6,alpha6,theta6)
-A7 = matriz_homogenea(d7,a7,alpha7,theta7)
 
-#Calculando os pontos de interesse e vetores no sistema Global
-T1 = A1
-T2 = T1@A2
-T3 = T2@A3
-T4 = T3@A4
-T5 = T4@A5
-T6 = T5@A6
-T7 = T6@A7
+for k in range(10):
+    ### parametros de DH variáveis
+    theta1 = pi/2 + q[0]
+    theta2 = q[1]
+    theta3 = q[2]
+    theta4 = q[3]
+    theta5 = q[4]
+    theta6 = pi/2 + q[5]
+    theta7 = pi/2 + q[6]
 
-v1 = T1[0:3,1]
-v2 = T2[0:3,1]
-v3 = T3[0:3,1]
-v4 = T4[0:3,1]
-v5 = T5[0:3,1]
-v6 = T6[0:3,1]
-v7 = vetor(T7[0:3,1])
+    #Calculando as matrizes homogêneas
+    A1 = matriz_homogenea(d1,a1,alpha1,theta1)
+    A2 = matriz_homogenea(d2,a2,alpha2,theta2)
+    A3 = matriz_homogenea(d3,a3,alpha3,theta3)
+    A4 = matriz_homogenea(d4,a4,alpha4,theta4)
+    A5 = matriz_homogenea(d5,a5,alpha5,theta5)
+    A6 = matriz_homogenea(d6,a6,alpha6,theta6)
+    A7 = matriz_homogenea(d7,a7,alpha7,theta7)
 
-#primeira interação
-pontos = Cinematica_Direta(q)
-proj = projecao_ponto_plano(v7,pontos[:,7],destino[:])
-v1 = proj - vetor(pontos[:,7])
-v2 = vetor(T7[0:3,2])
-v1 = v1/norm(v1)
-th = acosr(v1.T@v2)
-q[6,0] = q[6,0] + th
-q[5,0] = 95*(pi/180)
-q[4,0] = 80*(pi/180)
-pontos = Cinematica_Direta(q)
+    #Calculando os pontos de interesse e vetores no sistema Global
+    T1 = A1
+    T2 = T1@A2
+    T3 = T2@A3
+    T4 = T3@A4
+    T5 = T4@A5
+    T6 = T5@A6
+    T7 = T6@A7
+
+    #vetor
+    v1 = vetor(T1[0:3,1])
+    v2 = vetor(T2[0:3,1])
+    v3 = vetor(T3[0:3,1])
+    v4 = vetor(T4[0:3,1])
+    v5 = vetor(T5[0:3,1]) #y
+    v6 = vetor(T6[0:3,1])
+    v7 = vetor(T7[0:3,1]) #y
+
+    #primeira interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v7,pontos[:,7],destino[:])
+    va = proj - vetor(pontos[:,7])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v7,pontos[:,7],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,7])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v7,-pi/2)
+    if(vb.T@v < 0): th = -th
+    q[6,0] = q[6,0] + th
+
+
+
+    #segunda interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v6,pontos[:,6],destino[:])
+    va = proj - vetor(pontos[:,6])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v6,pontos[:,6],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,6])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v6,-pi/2)
+    if(vb.T@v < 0): th = -th
+    q[5,0] = q[5,0] + th
+
+    #terceira interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v5,pontos[:,5],destino[:])
+    va = proj - vetor(pontos[:,5])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v5,pontos[:,5],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,5])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v5,-pi/2)
+    if(vb.T@v < 0): th = -th
+    q[4,0] = q[4,0] + th
+
+
+    #quarta interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v4,pontos[:,4],destino[:])
+    va = proj - vetor(pontos[:,4])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v4,pontos[:,4],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,4])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v4,pi/2)
+    if(vb.T@v < 0): th = -th
+    q[3,0] = q[3,0] + th #estava -
+
+    #quinta interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v3,pontos[:,3],destino[:])
+    va = proj - vetor(pontos[:,3])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v3,pontos[:,3],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,3])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v3,-pi/2)
+    if(vb.T@v < 0): th = -th
+    q[2,0] = q[2,0] + th #estava - 
+
+    #sexta interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v2,pontos[:,2],destino[:])
+    va = proj - vetor(pontos[:,2])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v2,pontos[:,2],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,2])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v2,pi/2)
+    if(vb.T@v < 0): th = -th
+    q[1,0] = q[1,0] + th #estava -
+
+    #sétima interação
+    pontos = Cinematica_Direta(q)
+    print(distancia(pontos[:,8],destino,3))
+    proj = projecao_ponto_plano(v1,pontos[:,1],destino[:])
+    va = proj - vetor(pontos[:,1])
+    va = va/norm(va)
+    proj = projecao_ponto_plano(v1,pontos[:,1],vetor(pontos[:,8]))
+    vb = proj - vetor(pontos[:,1])
+    vb = vb/norm(vb)
+    th = acosr(va.T@vb)
+    v = rotationar_vetor(va,v1,-pi/2)
+    if(vb.T@v < 0): th = -th
+    q[0,0] = q[0,0] + th #estava -
+
+    pontos = Cinematica_Direta(q)
+    erro = distancia(pontos[:,8],destino,3) 
+    print(erro,'\n','fim da interacao:',k,'\n')
+    if(erro < 10**-5):
+        break
 
 plot(q,destino)
-plt.plot([destino[0,0],pontos[0,6]],[destino[1,0],pontos[1,6]],[destino[2,0],pontos[2,6]],'red')
-plt.plot([pontos[0,6],pontos[0,8]],[pontos[1,6],pontos[1,8]],[pontos[2,6],pontos[2,8]],'green')
+#plt.plot([destino[0,0],pontos[0,3]],[destino[1,0],pontos[1,3]],[destino[2,0],pontos[2,3]],'red')
+#plt.plot([pontos[0,3],pontos[0,8]],[pontos[1,3],pontos[1,8]],[pontos[2,3],pontos[2,8]],'green')
 #plt.plot([proj[0,0],pontos[0,7]],[proj[1,0],pontos[1,7]],[proj[2,0],pontos[2,7]],'green')
 #ax.scatter(proj[0,0],proj[1,0],proj[2,0],'green')
 plt.pause(1000)
