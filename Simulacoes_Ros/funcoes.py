ecf577e9d022b6c5a721bb8220261763fff44882
David1340 <davidnike80@gmail.com>
2021-09-01 16:00:10 -0300

    Criei algumas funções nova, para o fabrik corrigindo orientação

--- Simulacoes_Ros/funcoes.py
+++ Simulacoes_Ros/funcoes.py
@@ -273,6 +273,66 @@ def Cinematica_Direta2(q):
                         ,T7[0:3,1]]).T
     return [pontos,vetores]
 
+#Calcula a posição das juntas a partir da configuração q
+#e seus vetores de atuação
+def Cinematica_Direta3(q):
+    #Pontos de interesse
+    L = 0.075 #distância da ultima junta a extremidade do efetuador
+    p = np.array([[0,0,0,1]]).T #Base
+    p8_7 = np.array([[0,0,L,1]]).T #contato de atuação do efetuador
+
+    #Parâmetros Físicos do manipulador [m]
+    base = 0.05 #5 cm
+    #parametros de DH constantes
+    d1 = 0.075 + base
+    d2 = 0
+    d3 = 0.15
+    d4 = 0 
+    d5 = 0.145
+    d6 = 0
+    d7 = 0
+    a1 = 0
+    a2 = 0
+    a3 = 0
+    a4 = 0
+    a5 = 0
+    a6 = 0.075
+    a7 = 0
+    alpha1 = pi/2
+    alpha2 = -pi/2
+    alpha3 = pi/2
+    alpha4 = -pi/2
+    alpha5 = pi/2
+    alpha6 = pi/2
+    alpha7 = pi/2
+    theta1 = pi/2 + q[0]
+    theta2 = q[1]
+    theta3 = q[2]
+    theta4 = q[3]
+    theta5 = q[4]
+    theta6 = pi/2 + q[5]
+    theta7 = pi/2 + q[6]
+
+    #Calculando as matrizes homogêneas
+    A1 = matriz_homogenea(d1,a1,alpha1,theta1)
+    A2 = matriz_homogenea(d2,a2,alpha2,theta2)
+    A3 = matriz_homogenea(d3,a3,alpha3,theta3)
+    A4 = matriz_homogenea(d4,a4,alpha4,theta4)
+    A5 = matriz_homogenea(d5,a5,alpha5,theta5)
+    A6 = matriz_homogenea(d6,a6,alpha6,theta6)
+    A7 = matriz_homogenea(d7,a7,alpha7,theta7)
+
+    #Calculando os pontos de interesse no sistema Global
+    T1 = A1
+    T2 = T1@A2
+    T3 = T2@A3
+    T4 = T3@A4
+    T5 = T4@A5
+    T6 = T5@A6
+    T7 = T6@A7
+    p8_0 = T7@p8_7
+    return [p8_0[0:3,0] ,T7[0:3,0:3]]
+
 #Projeta um ponto em um plano
 def projecao_ponto_plano(normal,p0,p):
     #normal -> vetor normal ao plano
